23fdfd4359a62ec1942fd827bd12c1295a60cbe6
Jon Colverson <github@vcxz.co.uk>
2016-08-28 21:48:01 +0100

    Renamed `__main__.py` to `send.py`

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setuptools.setup(
     name = 'remotedownload',
     version = '1.0.0dev',
     packages = setuptools.find_packages(),
-    entry_points = {'console_scripts': ['remotedownloadsend = remotedownload.__main__:send']},
+    entry_points = {'console_scripts': ['remotedownloadsend = remotedownload.send:send']},
 )
