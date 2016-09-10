dbcb86af2bdeabe2c7b32de5a123ab0843fea6ea
Jon Colverson <github@vcxz.co.uk>
2016-09-10 19:09:08 +0100

    Made this project just for the FlashGot interface by renaming things and removing the downloading code

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 import setuptools
 
 setuptools.setup(
-    name = 'remotedownload',
+    name = 'remotedownload-flashgot',
     version = '1.0.0dev',
     packages = setuptools.find_packages(),
     entry_points = {'console_scripts': [
-        'remotedownloadsend = remotedownload.send:main',
-        'remotedownloadget = remotedownload.get:main',
+        'remotedownload-flashgot = remotedownloadflashgot.__main__:main',
     ]},
-    install_requires = ['requests'],
 )
